d24137b6cbe6e4ab7d0db4740bef96be9fa34046
BumgeunSong <isp1195@gmail.com>
2022-05-03 16:52:00 +0900

    Create initial projects

--- Krello/RootViewController.swift
+++ Krello/RootViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
 
---
+++ Podfile
@@ -0,0 +1,14 @@
+target 'Krello' do
+  use_frameworks!
+
+  # Pods for Krello
+  pod 'SwiftLint'
+
+  target 'KrelloTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'SwiftLint'
+
+  end
+
+end
