e7eb371deb548b91aedc9158383ff8fbc8ff035b
BumgeunSong <isp1195@gmail.com>
2022-05-06 16:06:47 +0900

    [#17] Delete: Unneseccary logic in testing app delegate

--- Krello/Supporting/TestingAppDelegate.swift
+++ Krello/Supporting/TestingAppDelegate.swift
@@ -9,8 +9,8 @@ import UIKit
 
 @objc(TestingAppDelegate)
 class TestingAppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        print("✅ Launching with testing app delegate")
-        return true
-    }
+
+    // 만약 테스트 타겟에서 DB나 설정들이 필요하다면 application(_:didFinishLaunchingWithOptions:) 추가
+    // 필요 없는 경우 비어있는 상태로 놔둠.
+
 }
