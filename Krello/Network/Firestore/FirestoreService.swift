ac81cd3b8049df12456ce2e102282347eb8a4776
Jinsujin <sylpid003@gmail.com>
2022-05-12 00:18:11 +0900

    [#27] Feat: FirestoreService 구현
    Firestore 에서 데이터를 가져와 Model 로 디코딩하는 역할인 FirestoreService 를 구현

---
+++ Krello/Network/Firestore/FirestoreService.swift
@@ -0,0 +1,154 @@
+//
+//  FirestoreService.swift
+//  Krello
+//
+//  Created by Sujin Jin on 2022/05/10.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+struct ServerUser: Identifiable, Codable {
+    @DocumentID var id: String?
+    let name: String
+    let uid: String // 회원가입할때 생성된 uid
+}
+
+struct BoardTask: Codable, Hashable {
+    let uid: String
+    let title: String
+    let status: TaskStatus
+    let contents: String
+    let rowPosition: Int
+    let createdAt: Date
+}
+
+enum TaskStatus: String, Codable {
+    case todo = "Todo"
+    case inprogress = "Inprogress"
+    case done = "Done"
+    case none = "None"
+}
+
+struct ServerBoard: Identifiable, Codable {
+    @DocumentID var id: String?
+    let ownerUid: String
+    let title: String
+    var tasks: [BoardTask]?
+}
+
+struct ServerLog: Identifiable, Codable {
+    @DocumentID var id: String?
+    let taskTitle: String
+    let createdAt: Date
+    let action: LogAction
+}
+
+enum LogAction: String, Codable {
+    case move = "Move"
+    case add = "Add"
+    case delete = "Delete"
+    case update = "Update"
+}
+
+enum FirestoreServiceError: Error {
+    case notDecodeData
+    case emptyData
+    case error(error: Error)
+}
+
+final class FirestoreService {
+
+    func fetchUser(email: String, _ completion: @escaping (Result<ServerUser, FirestoreServiceError>) -> Void) {
+        let db = Firestore.firestore()
+        let userRef = db.collection("users").document(email)
+
+        userRef.getDocument { (documentSnapshot, error) in
+            if let error = error {
+                completion(.failure(.error(error: error)))
+                return
+            }
+            guard let document = documentSnapshot, document.exists else {
+                completion(.failure(.emptyData))
+                return
+            }
+            guard let decodedModel = try? document.data(as: ServerUser.self) else {
+                completion(.failure(.notDecodeData))
+                return
+            }
+            completion(.success(decodedModel))
+        }
+    }
+
+    func fetchAllUsers(_ completion: @escaping (Result<[ServerUser], FirestoreServiceError>) -> Void) {
+        let db = Firestore.firestore()
+        let usersRef = db.collection("users")
+
+        usersRef.getDocuments { (querySnapshot, error) in
+            if let error = error {
+                completion(.failure(.error(error: error)))
+                return
+            }
+
+            guard let documents = querySnapshot?.documents else {
+                completion(.failure(.emptyData))
+                return
+            }
+
+            var results = [ServerUser]()
+            for document in documents {
+                guard let decodeModel = try? document.data(as: ServerUser.self) else {
+                    continue
+                }
+                results.append(decodeModel)
+            }
+            completion(.success(results))
+        }
+    }
+
+    func fetchBoards(userUid: String, _ completion: @escaping (Result<[ServerBoard], FirestoreServiceError>) -> Void) {
+        let db = Firestore.firestore()
+        let boardRef = db.collection("boards").whereField("ownerUid", isEqualTo: userUid)
+
+        boardRef.getDocuments { (querySnapshot, error) in
+            if let error = error {
+                completion(.failure(.error(error: error)))
+                return
+            }
+            var result = [ServerBoard]()
+            for document in querySnapshot!.documents {
+                guard let model = try? document.data(as: ServerBoard.self) else {
+                    continue
+                }
+                result.append(model)
+            }
+            completion(.success(result))
+        }
+    }
+
+    func fetchLogs(boardUid: String, _ completion: @escaping(Result<[ServerLog], FirestoreServiceError>) -> Void) {
+        let db = Firestore.firestore()
+        let boardLogsRef = db.collection("boards").document(boardUid).collection("logs")
+
+        boardLogsRef.getDocuments { (snapShot, error) in
+            if let error = error {
+                completion(.failure(.error(error: error)))
+                return
+            }
+            guard let documents = snapShot?.documents else {
+                completion(.failure(.emptyData))
+                return
+            }
+            var result = [ServerLog]()
+            for document in documents {
+                guard let decodeModel = try? document.data(as: ServerLog.self) else {
+                    continue
+                }
+                result.append(decodeModel)
+            }
+            completion(.success(result))
+        }
+    }
+}
