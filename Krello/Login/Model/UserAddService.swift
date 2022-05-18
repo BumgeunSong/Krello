f0c7cdeedb998ccc48b37fe04527fef6a5657b3f
BumgeunSong <isp1195@gmail.com>
2022-05-18 15:50:06 +0900

    Add: AddService & GetService (temp)

---
+++ Krello/Login/Model/UserAddService.swift
@@ -0,0 +1,33 @@
+//
+//  UserAddService.swift
+//  Krello
+//
+//  Created by Bumgeun Song on 2022/05/17.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+protocol UserAdding {
+    func addUser(uid: String, email: String, userName: String, _ completion: @escaping () -> Void)
+}
+
+struct UserAddService: UserAdding {
+    func addUser(uid: String, email: String, userName: String, _ completion: @escaping () -> Void) {
+        let db = Firestore.firestore()
+        let userRef = db.collection("users").document(uid)
+        userRef.setData([
+            "id": uid,
+            "email": email,
+            "name": userName
+        ]) { err in
+            if let err = err {
+                print(err)
+            } else {
+                completion()
+            }
+        }
+    }
+}
