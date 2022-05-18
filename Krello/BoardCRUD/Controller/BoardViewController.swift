8c0543deaccd966170d388d4c2ccdb1018fd6ea2
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-05-18 16:53:42 +0900

    [#51] Refactor: Task class to conform  NSItemProviderWriting, NSItemProviderReading

--- Krello/BoardCRUD/Controller/BoardViewController.swift
+++ Krello/BoardCRUD/Controller/BoardViewController.swift
@@ -9,14 +9,7 @@ import UIKit
 
 class BoardViewController: UIViewController {
     let dummyStatus = ["Todo", "In progress", "Done"]
-    let dummyTasks = [
-        ["Group 구조 기능 단위로 정리하기", "BoardViewController 구현하기"],
-        ["로그인 UI 구현", "회원가입 UI 구현", "회원 가입 Validation 로직 구현", "Github Action 에러 해결", "UIKit Preview 기능 추가", "[할일 조회] FirestoreService 구현", "TableView 높이 해결", "Firestore 에서 데이터를 읽어오는 객체 구현 & BoardList 화면에 데이터 뿌리기", "login view 터치했을때 키보드가 내려갔는지 테스트", "AuthenticationManger 추가"],
-        ["Pull Request, Issue 템플릿 적용"]
-    ]
-
     var boardName: String = "Dummy Board"
-
     var boardView: BoardView?
 
     override func viewDidLoad() {
