77c670de808982dbcd0c388fe8d38579027ca498
BumgeunSong <isp1195@gmail.com>
2022-05-19 20:47:30 +0900

    Fix: conflict resolve in Task model

--- Krello/Login/Model/Task.swift
+++ Krello/Login/Model/Task.swift
@@ -7,22 +7,59 @@
 
 import Foundation
 import FirebaseFirestoreSwift
+import MobileCoreServices
 
-struct Task: Identifiable, Codable, Hashable {
-    let id: FirestoreUID
+final class Task: NSObject, Identifiable, Codable, NSItemProviderReading, NSItemProviderWriting {
+    static var readableTypeIdentifiersForItemProvider: [String] {
+        return [String(kUTTypeData)]
+    }
+    static var writableTypeIdentifiersForItemProvider: [String] {
+        return [String(kUTTypeData)]
+    }
+
+    func loadData(withTypeIdentifier typeIdentifier: String, forItemProviderCompletionHandler completionHandler: @escaping (Data?, Error?) -> Void) -> Progress? {
+            let progress = Progress(totalUnitCount: 100)
+            do {
+                let data = try JSONEncoder().encode(self)
+                progress.completedUnitCount = 100
+                completionHandler(data, nil)
+            } catch {
+                completionHandler(nil, error)
+            }
+            return progress
+        }
+
+    static func object(withItemProviderData data: Data, typeIdentifier: String) throws -> Task {
+        do {
+            let subject = try JSONDecoder().decode(Task.self, from: data)
+            return subject
+        } catch {
+           throw DragAndDropError.DropDecodingErrror
+        }
+    }
+
+    let id: String
     let title: String
     let status: Status
     let contents: String
     let rowPosition: Int
     let createdAt: Date
 
+    init(id: String, title: String, status: Status, contents: String, rowPosition: Int, createdAt: Date) {
+        self.id = id
+        self.title = title
+        self.contents = contents
+        self.status = status
+        self.rowPosition = rowPosition
+        self.createdAt = createdAt
+    }
+
     enum Status: String, Codable {
         case todo = "Todo"
-        case inprogress = "Inprogress"
+        case inprogress = "In progress"
         case done = "Done"
         case none = "None"
     }
-
     var serialized: [String: Any] {
         return [
             "id": id,
@@ -33,4 +70,8 @@ struct Task: Identifiable, Codable, Hashable {
             "createdAt": createdAt
         ]
     }
+}
+
+enum DragAndDropError: Error {
+    case DropDecodingErrror
 }
